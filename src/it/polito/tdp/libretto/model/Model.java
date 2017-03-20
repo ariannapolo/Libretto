99730cb0b256d56dcc52dedc042869aa07c03841
Arianna <Arianna@172.20.94.35>
2017-03-20 10:43:31 +0100

    Class Model

--- src/it/polito/tdp/libretto/model/Model.java
+++ src/it/polito/tdp/libretto/model/Model.java
@@ -1,5 +1,41 @@
 package it.polito.tdp.libretto.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
+	private List<Esame> esami;
+	
+	public Model(){
+		this.esami = new ArrayList<Esame>();
+	}
+	/**
+	 * Aggiunge un nuovo esame agli esami presenti,
+	 * verificando che non ci sia già
+	 * @param e
+	 * @return true se l'ha inserito, false se esisteva già e quindi non è stato inserito
+	 */
+	public boolean addEsame(Esame e){
+		if(!esami.contains(e)){ //qui ci serve equals
+			esami.add(e);
+			return true;
+		}else
+			return false;
+	}
+	/**
+	 * Ricerca se esiste un esame con il codice specificato.
+	 * Se esiste lo restituisce, altrimenti restituisce null.
+	 * @param codice codice dell'esame da cercare
+	 * @return l'esame trovato, oppure null se non trovato
+	 */
+	public Esame trovaEsame(String codice){
+		//creo un oggetto esame "finto" con quel codice
+		int pos = esami.indexOf(new Esame(codice, null, null));
+		if(pos == -1){
+			return null;
+		}else{
+			return esami.get(pos);
+		}
+	}
 
 }
